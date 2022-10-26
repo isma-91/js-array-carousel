682e66d52d04089b6c1c4842c56ffdc0a0011914
isma-91 <isma91@hotmail.it>
2022-10-26 16:48:18 +0200

    Add all javascript but loop doesn't work

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 const eleSlider = document.querySelector(".container");
-const eleArrDn = document.querySelector(".arrow-up");
-const eleArrUp = document.querySelector(".arrow-down");
+const eleArrDn = document.querySelector(".arrow-down");
+const eleArrUp = document.querySelector(".arrow-up");
 const arrImages = [
   "img/01.jpg",
   "img/02.jpg",
@@ -17,5 +17,45 @@ for (let i = 0; i < arrImages.length; i++) {
   if (i === 0) {
     eleImg.classList.add("visible");
   }
+
   eleSlider.append(eleImg);
 }
+
+let visibleValue = 0;
+
+const listImgs = document.querySelectorAll(".slider-img");
+
+eleArrDn.addEventListener("click", function () {
+  listImgs[visibleValue].classList.remove("visible");
+
+  visibleValue++; //visibleValue = visibleValue + 1
+
+  listImgs[visibleValue].classList.add("visible");
+
+  eleArrUp.classList.remove("hidden");
+  // if (visibleValue === listImgs.length - 1) {
+  //   eleArrDn.classList.add("hidden");
+  // }
+
+  if (visibleValue === listImgs.length - 1) {
+    listImgs[visibleValue].classList.remove("visible");
+    listImgs[(visibleValue = 0)].classList.add("visible");
+  }
+});
+
+eleArrUp.addEventListener("click", function () {
+  listImgs[visibleValue].classList.remove("visible");
+
+  visibleValue--; //visibleValue = visibleValue + 1
+
+  listImgs[visibleValue].classList.add("visible");
+
+  // if (visibleValue === 0) {
+  //   eleArrUp.classList.add("hidden");
+  // }
+  if (visibleValue > 0) {
+    listImgs[visibleValue].classList.remove("visible");
+    listImgs[(visibleValue = arrImages.length - 1)].classList.add("visible");
+  }
+  eleArrDn.classList.remove("hidden");
+});
