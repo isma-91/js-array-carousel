177e1ef0a0b29ef3160f252f7b67e9e2bec47675
isma-91 <isma91@hotmail.it>
2022-10-26 15:39:20 +0200

    Trying to add the js cycle

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+const eleSlider = document.querySelector(".container");
+const eleArrDn = document.querySelector(".arrow-up");
+const eleArrUp = document.querySelector(".arrow-down");
+const arrImages = [
+  "img/01.jpg",
+  "img/02.jpg",
+  "img/03.jpg",
+  "img/04.jpg",
+  "img/05.jpg",
+];
+
+for (let i = 0; i < arrImages.lenght; i++) {
+  const eleImg = document.createElement("img");
+  eleImg.src = arrImages[i];
+  eleImg.classList.add("slider-img");
+  console.log(eleImg);
+
+  eleSlider.append(eleImg);
+}
